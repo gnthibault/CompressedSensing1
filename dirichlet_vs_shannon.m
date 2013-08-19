96b5189f115c540b5343f15e09d1ae05972e26bf
Thibault Notargiacomo <gnthibault@users.noreply.github.com>
2013-08-19 22:01:00 +0200

    Update dirichlet_vs_shannon.m
    Added the upsampling in fourier domain (that resulted in poor performances ...) for comparison

--- dirichlet_vs_shannon.m
+++ dirichlet_vs_shannon.m
@@ -3,13 +3,12 @@ clear all;
 close all;
 
 
-
-Fe = 3005;
+Fe = 3250;
 Te = 1/Fe;
 Nech = 100;
 
-F1 = 500;
-F2 = 1000;
+F1 = 512;
+F2 = 625;
 FMax = 1500;
 
 time = [0:Te:(Nech-1)*Te];
@@ -49,9 +48,27 @@ TimeInterpDiscrete = [1:1:NechInterp];
 %Compute original signal value without any interpolation
 signalResampled = cos(2*pi*F1*(TimeInterp))+cos(2*pi*F2*(TimeInterp))+cos(2*pi*FMax*(TimeInterp));
 
-%Compute original signal interpolation through patlab resample function
+%Compute original signal interpolation through matlab resample function
 [P,Q] = rat(Finterp/Fe);
 interp_matlab = resample(reconstruction,P,Q);
+
+%Compute original signal interpolation by padding the fft and performing
+%inverse fft on the result
+semipaddedsize=floor(NechInterp/2);
+padded_spectrum0 = zeros(1,semipaddedsize);
+padded_spectrum0 = padarray(spectrum(1:Nech/2),[0 semipaddedsize-(Nech/2)],0,'post');
+padded_spectrum = zeros(1,NechInterp);
+padded_spectrum(1:semipaddedsize) = padded_spectrum0;
+padded_spectrum(semipaddedsize+1:NechInterp-1) = conj(fliplr(padded_spectrum0));
+padded_timeDiscrete = [1:1:NechInterp];
+padded_reconstruction = zeros(1,NechInterp);
+for k = padded_timeDiscrete
+    for l = padded_timeDiscrete
+        padded_reconstruction(k) = padded_reconstruction(k) + padded_spectrum(l)*exp(2*pi*j*l*k/NechInterp);
+    end
+end
+padded_reconstruction=padded_reconstruction/(1*Nech);
+
 
 %Compute original signal interpolation through shannon interpolation method
 interp_shannon=zeros(1,NechInterp);
@@ -82,12 +99,27 @@ interp_dirichlet = interp_dirichlet/NechInterp;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % figure(1);
-% plot(time,signal);
+% plot(time,signal,'g');
 % hold on;
 % plot(time,real(reconstruction),'r');
-% 
-% figure(2);
+ 
+spectrumresampled=zeros(1,NechInterp);
+for k = TimeInterpDiscrete
+    for l = TimeInterpDiscrete
+        spectrumresampled(k) = spectrumresampled(k) + signalResampled(l)*exp(-2*pi*j*l*k/NechInterp);
+    end
+end
+
+figure(2);
+plot(abs(spectrumresampled)/6,'g');
+% hold on;
+% plot(abs(fft(padded_reconstruction)),'r');
+ hold on;
+ plot(abs(padded_spectrum),'b');
+% hold on;
+% plot(abs(spectrum)*5,'c');
 % plot(frequency(1:Nech/2),abs(spectrum(1:Nech/2)));
+% plot(frequency,asbs(spectrum));
 
 
 figure(3);
@@ -96,10 +128,13 @@ figure(3);
 plot(TimeInterp,signalResampled,'g');
 hold on;
 % linear interpolation between subsampled points (matlab tracing tool)
-plot(time,real(reconstruction),'c');
+plot(time,(reconstruction),'c');
 hold on;
 % matlab resample command interpolation
 plot(TimeInterp,real(interp_matlab(1:NechInterp)),'r');
+hold on;
+% Padding the spectrum and reconstructing it 
+plot(TimeInterp,real(padded_reconstruction),'m');
 hold on;
 % Shannon interpolation method
 plot(TimeInterp,real(interp_shannon),'b');
@@ -110,12 +145,15 @@ plot(TimeInterp,real(interp_dirichlet),'k');
 xlabel('Time in s','FontSize',16)
 ylabel('Signal value (no unit)','FontSize',16)
 title('\it{ Various signal reconstruction from fourier transform }','FontSize',16)
-legend('True signal', 'Reconstruction with linear interpolation','Reconstruction with matlab resample function', 'Reconstruction with Shannon interpolation','Reconstruction with Dirichlet interpolation');
+legend('True signal', 'Reconstruction with linear interpolation','Reconstruction with matlab resample function', 'Reconstruction with padded spectrum', 'Reconstruction with Shannon interpolation','Reconstruction with Dirichlet interpolation');
 
 figure(4);
 
 % matlab resample command interpolation
 plot(TimeInterp,real(interp_matlab(1:NechInterp))-signalResampled,'r');
+hold on;
+% Padding the spectrum and reconstructing it 
+plot(TimeInterp,real(padded_reconstruction)-signalResampled,'m');
 hold on;
 % Shannon interpolation method
 plot(TimeInterp,real(interp_shannon)-signalResampled,'b');
@@ -126,4 +164,4 @@ plot(TimeInterp,real(interp_dirichlet)-signalResampled,'k');
 xlabel('Time in s','FontSize',16)
 ylabel('Error regarding true signal value (no unit)','FontSize',16)
 title('\it{ Interpolation Error }','FontSize',16)
-legend('Reconstruction with matlab resample function', 'Reconstruction with Shannon interpolation', 'Reconstruction with Dirichlet interpolation');
+legend('Reconstruction with matlab resample function', 'Reconstruction with padded spectrum', 'Reconstruction with Shannon interpolation', 'Reconstruction with Dirichlet interpolation');
